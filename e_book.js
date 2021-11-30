4f4708c4a881174d795aba0203a1dbe16fedc074
fatimasabah <fatimasabahnahi@gmail.com>
2021-11-30 23:07:49 +0300

    first comment

---
+++ e_book.js
@@ -0,0 +1,23 @@
+// sidebar
+let btn =document.querySelector('#btn-menu');
+let sidebar=document.querySelector('.sidebar');
+let searchbtn=document.querySelector('.bx-search');
+
+btn.onclick =function(){
+    sidebar.classList.toggle("active")
+}
+
+searchForm=document.querySelector('.search-form');
+document.querySelector ('#search-btn').onclick =() =>{
+    searchForm.classList.toggle('active');
+
+}
+
+let loginForm =document.querySelector('.login-form-contianer');
+document.querySelector('#login-btn').onclick =() =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick =() =>{
+    loginForm.classList.remove('active');
+}
